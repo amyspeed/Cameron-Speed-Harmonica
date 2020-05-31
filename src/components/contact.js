c424a096af72753da146f3903cb2e920a7cc2dda
Amy Speed <amy.speed.henley@gmail.com>
2020-05-31 02:48:22 -0500

    Placeholder for contact method

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+
+import './contact.css';
+
 const Contact = () => {
     return(
         <div>
             <div className="page-content">
-                <form>
+                <a className="send-link-wrap" href="mailto:cameron.l.speed@gmail.com?subject=Mail from Website">
+                    <FontAwesomeIcon icon={faPaperPlane} className="plane-icon" />
+                </a>
+                {/* <form>
 
-                </form>
+                </form> */}
             </div>
         </div>
     )
