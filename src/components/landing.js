9feba5ae3f90d7cfb374d1798e41cc797f4b0252
Amy Speed <amy.speed.henley@gmail.com>
2020-06-17 21:44:04 -0500

    comment unused const

--- src/components/landing.js
+++ src/components/landing.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import smoothscroll from 'smoothscroll-polyfill';
-import Spinner from 'react-spinkit';
+// import Spinner from 'react-spinkit';
 
 import './landing.css';
 
